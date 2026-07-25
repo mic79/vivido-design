829446e697564d20eba0453fad3e005cd2228530
Michiel <michiel79@gmail.com>
2026-07-24 20:02:56 -0400

    Added body-riggedGD v4.

--- webxr/body-riggedGD/index.service.worker.js
+++ webxr/body-riggedGD/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784935893|2192790';
+const CACHE_VERSION = '1784937433|2096486';
 /** @type {string} */
 const CACHE_PREFIX = 'Sci-fi Starter K-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
